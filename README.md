730d688d07e167a914d2be31969d56ac7429135f
Sebastian Spicker <sebastian.spicker@googlemail.com>
2023-10-29 09:21:00 +0100

    docs: collect the first CS2-specific optimization hypotheses

---
+++ README.md
@@ -0,0 +1,12 @@
+# cs2-opt
+
+A working tree for cs2-opt with an evolving implementation history.
+
+## Overview
+cs2-opt is moving through bootstrap push work.
+
+## Status
+Working state: bootstrap.
+
+## Usage
+- Made the the main flow assumptions easier to check later.
---
+++ tests/Main.Tests.ps1
@@ -0,0 +1,10 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+# collect the first cs2 specific optimization hypotheses entrypoint
+
+# current lane: collect_the_first_cs2_specific_optimization_hypotheses
+function Invoke-CollectTheFirstCs2SpecificOptimizationHypotheses {
+    [CmdletBinding()]
+    param()
+}
